27357561ab2509715c479101b2ccabb22599579d
Leonardo Foletto <flttleonardo@gmail.com>
2021-03-06 16:47:55 +0100

    changed prepro to scipy.io.wavfile

--- App/Modules/PreprocessingSample.py
+++ App/Modules/PreprocessingSample.py
@@ -1,5 +1,6 @@
 import librosa
-import soundfile
+from scipy.io.wavfile import write
+
 
 def pitchshift(folder,file,shifts=24):
     print('loading audio')
@@ -8,4 +9,5 @@ def pitchshift(folder,file,shifts=24):
     root = 40 # e2 is 40
     for i in range(0 - shifts//2,(shifts//2)+1):
         y_shift = librosa.effects.pitch_shift(y,sr,i,bins_per_octave=12)
-        soundfile.write("{}{}.wav".format(folder, i+root), y_shift, sr)
+        #write("{}{}.wav".format(folder, i+root), y_shift, sr)
+        write(f'{root+i}.wav', sr, y_shift) 
