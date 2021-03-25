486df17bc2bb2fa1afa07eb2cc24eee161686c7b
canvas123 <canvas123@protonmail.com>
2021-03-26 02:45:36 +0300

    Resolved #17

--- common/customw/basic.py
+++ common/customw/basic.py
@@ -1,7 +1,16 @@
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.properties import StringProperty, ObjectProperty
 
 
 class Graphics(Widget):
 
-    def midi_text(self):
-        self.parent.parent.ids.lab.text = 'please drag and drop a .mid file here'
+    midi_file = StringProperty("")
+
+    def __init__(self, **kwargs):
+        Window.bind(on_dropfile=self._on_file_drop)
+        super(Graphics, self).__init__(**kwargs)
+
+    def _on_file_drop(self, window, file_path):
+        self.midi_file = file_path.decode()
