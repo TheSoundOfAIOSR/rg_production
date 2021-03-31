26a2f49f0901ad37a918897fcd20f1b6e15127df
Jesse Hill <j_hill14@u.pacific.edu>
2021-03-30 20:27:06 -0400

    add init va

--- common/state/state_manager.py
+++ common/state/state_manager.py
@@ -1,17 +1,31 @@
 from kivy.event import EventDispatcher
 from kivy.app import App
 import common.log as log
+from enum import Enum
 
 logger = log.setup_logger()
-class EventManager(EventDispatcher):
-    def __init__(self, **kwargs):
-        self.register_event_type('on_critical_button_pressed')
-        super(EventManager, self).__init__(**kwargs)
 
-    def on_critical_button_pressed(self, *args):
-        pass
+class StateEnum(Enum):
+    Loading = 1
+    Update = 2
+    Playing_Idle = 3
+    Recording = 4
+    New_Sound_Generation = 5
 
-class StateManager(EventManager):
-
+class StateManager(EventDispatcher):
     def __init__(self, **kwargs):
         super(StateManager, self).__init__(**kwargs)
+        self.register_event_type('on_critical_button_pressed')
+
+        """Application Variables"""
+        self.text = None
+        self.last_transcribed_text = None
+        self.sound_descriptor = None
+        self.state = StateEnum.Loading
+
+    def on_critical_button_pressed(self, *args):
+        """
+        Perform an action when "Record" or "Generate" is pressed
+
+        """
+        logger.debug("args")
