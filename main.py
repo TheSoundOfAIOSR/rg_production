c9e08b6146eacdd5622f18b3f9cff892fe9fd099
Jesse Hill <j_hill14@u.pacific.edu>
2021-04-09 11:12:28 -0400

    Clean up main

--- main.py
+++ main.py
@@ -53,17 +53,9 @@ class ProdApp(App):
         are finished
         """
         self.sm = StateManager()
-        # self.ws_connection_loops = asyncio.ensure_future(self.sm.stt.run())
         self.other_task = asyncio.ensure_future(self.main_loop())
 
-        # asyncio.ensure_future(self.sm.stt.run()) # self.sm.tts.run() self.sm.sg.run()
-        # self.other_task = asyncio.ensure_future(self.main_loop())
-        # self.sm.tts = ws.TTS
-        # self.sm.stt =  ws.STTClient(host=config.host, port=config.base_port)
-        # self.sm.sg = ws.SGClient(host=config.host, port=config.base_port + 2)
         async def run_wrapper():
-            # we don't actually need to set asyncio as the lib because it is
-            # the default, but it doesn't hurt to be explicit
             await self.async_run(async_lib="asyncio")
             self.csound.cleanup()  # before terminating the app, do the cleanup for Csound
             logger.info("App done")
@@ -75,8 +67,6 @@ class ProdApp(App):
     async def main_loop(self):
         await asyncio.sleep(5) # This is so
 
-        # print("sleeping")
-        # print("setting up models")
         try:
             await self.sm.stt.setup_model()
         except:
@@ -90,37 +80,8 @@ class ProdApp(App):
         self.csound.compile_and_start()
         self.csound.start_perf_thread()
 
-        # await asyncio.sleep(5)
-        # logger.debug(await self.stt_client.status())
-        # logger.debug(await self.stt_client.setup_model())
-        # logger.debug(await self.stt_client.status())
-        # logger.debug(await self.stt_client.start('1'))
-        # res = await self.stt_client.setup()
-
-        # arr = res['resp'][0]
-        # f = np.array(arr)
-        # preprocess(folder, f, 60, 48)
-
-        # await self.sm.setup_models()
-
-
-
-        # TODO: Move to WS_INIT event??
-
-        # self.startup = asyncio.create_task(self.sm.setup_models())
-        # self.sm.dispatch('on_pipeline_action', {'action':'pipeline_action_connect_ws'})
-        # await self.startup
-        folder = self.csound.audio_dir.as_posix()
-        sample = self.csound.sample_path.as_posix()
-
-        # await asyncio.sleep(1000000)
-        # TODO: Turn on preprocess in config, and if it's done in preprocess write false to preprocess field in config.json
-        # because we don't want to preprocess twice (it take at least 20 seconds to do so)
-        # if self.config.preprocess:
-        #     preprocess(folder, sample, 40, 48)
-
-        """This method is also run by the asyncio loop and periodically prints
-        something.
+        """
+        This is the main loop which the application will run inside of.
         """
         try:
             while True:
