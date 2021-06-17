3af27761cc73b00b551fe2037eee1e08bb5feaef
Jesse Hill <j_hill14@u.pacific.edu>
2021-06-17 15:06:46 -0400

    Load servers before gui init

--- common/clients/wsclient.py
+++ common/clients/wsclient.py
@@ -2,8 +2,20 @@ from rgws.interface import WebsocketClient
 import asyncio
 
 class STTClient(WebsocketClient):
-    def __init__(self, **kwargs):
+    def __init__(self, module, **kwargs):
         super(STTClient, self).__init__(**kwargs)
+        self.module = module
+        self.setup = False
+
+    def handle_task_result(self, task, ws=None):
+        # print(ws)
+        try:
+            task.result()
+            self.setup = True
+        except asyncio.CancelledError:
+            pass
+        except Exception:
+            print("Error in setup")
 
     async def _producer(self, websocket):
 
