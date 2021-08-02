b2e7ce0daf9977e4a542c5866c719dcfeba69fa4
Jesse Hill <j_hill14@u.pacific.edu>
2021-08-02 11:11:32 -0400

    wsclient update with rgws

--- common/clients/wsclient.py
+++ common/clients/wsclient.py
@@ -7,36 +7,24 @@ class STTClient(WebsocketClient):
         self.module = module
         self.setup = False
 
-    def handle_task_result(self, task, ws=None):
-        # print(ws)
-        try:
-            task.result()
-            self.setup = True
-        except asyncio.CancelledError:
-            pass
-        except Exception:
-            print("Error in setup")
+    async def startup(self):
+        print(f"Setting up {self.module}...")
+        asyncio.create_task(self.run())
 
-    async def _producer(self, websocket):
+        await asyncio.sleep(5)
+
+        i = 1
 
         while True:
-            await asyncio.sleep(100)
+            await asyncio.sleep(1)
+            try:
+                await self.status()
+                break
+            except:
+                pass
 
-class TTSClient(WebsocketClient):
-    def __init__(self, **kwargs):
-        super(TTSClient, self).__init__(**kwargs)
-
-    async def _producer(self, websocket):
-        while True:
-            await asyncio.sleep(100)
-
-        # logging.debug(await self.example_func("blo"))
-        # if you want to pass function with arguments, you can use functools.partial(func, args)
-        # logging.debug(await self.read_data_stream(websocket, self.stream_func))
-
-class SGClient(WebsocketClient):
-    def __init__(self, **kwargs):
-        super(SGClient, self).__init__(**kwargs)
+        if self.module == "stt":
+            await self.setup_model()
 
     async def _producer(self, websocket):
         while True:
